3ebbf6c9bd6c95cf82aab669a0ddb1ce41754962
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2020-07-07 21:32:26 +0200

    add structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct Date {
+    day: u8,
+    month: u8,
+    year: u8,
+}
+
+struct Item {
+    name: String,
+    description: String,
+    expires_on: Date,
+    bought_on: Date,
+}
+
+struct List {
+    items: Vec<Item>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
